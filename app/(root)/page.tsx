61ae0e6e7cd9894546b0f78f6b79aa79f218c554
Xie Kaiwen <xiekaiwen3@gmail.com>
2024-07-06 19:15:42 +0800

    Implement the layout fully

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import HeaderBox  from '@/components/HeaderBox';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
 export default function Home(){
     const loggedIn = {
-        firstName: "Kaiwen"
+        firstName: "Kaiwen",
+        lastName: "Xie",
+        email: "xiekaiwen3@gmail.com"
     }
     return(
         <section className='home'> 
@@ -23,7 +26,14 @@ export default function Home(){
                         totalCurrentBalance={1250.35}
                     />
                 </header>
+                RECENT TRANSACTIONS
             </div>
+
+            <RightSidebar 
+                user={loggedIn}
+                transactions = {[]}
+                banks = {[{currentBalance: 123.50}, {currentBalance: 500}]}
+            />
         </section>
     )
 }
